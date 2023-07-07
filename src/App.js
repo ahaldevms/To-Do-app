e7293de25245b3e5f2610e7559aac3309a8d345b
Ahal Dev MS <ahal.vfx@gmail.com>
2023-07-07 12:39:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-
+import React,{useState} from 'react';
+import './App.css';
 
 function App() {
+  const[toDos,setTodos] = useState([])
+  const[toDo,setTodo] = useState('')
+
+
   return (
-    <h1>Hello World</h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,toDo])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input  type="checkbox" name="" id="" />
+            <p>React tutorial</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
